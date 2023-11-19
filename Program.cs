d79908df33617dd7b54a3683281d38d2de057f02
GabrielFim <gabifimtm@gmail.com>
2023-11-19 17:56:17 -0300

    Rabbit?

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System.Net;
 using MPI;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
 
 namespace SnapshotChat
 {
@@ -12,20 +15,30 @@ namespace SnapshotChat
                 Console.WriteLine($"########## Process: {comm.Rank} ##########");
                 var receiveHandler = HandleReceive(comm);
                 var sendHandler = HandleSend(comm);
-                var snapshotHandler = HandleSnapshot(comm);
+                //var snapshotHandler = HandleSnapshot(comm);
 
                 receiveHandler.Start();
                 sendHandler.Start();
-                snapshotHandler.Start();
+                //snapshotHandler.Start();
 
                 receiveHandler.Wait();
                 sendHandler.Wait();
-                snapshotHandler.Wait();
+                //snapshotHandler.Wait();
             });
         }
 
         private static Task HandleSend(Intracommunicator comm) => new Task(() =>
         {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "msgs",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
             while (true)
             {
                 var input = Console.ReadLine();
@@ -35,20 +48,51 @@ namespace SnapshotChat
                 Console.SetCursorPosition(0, currentCursorLine);
                 if (!string.IsNullOrEmpty(input))
                 {
+                    /*
                     for (int i = 0; i < comm.Size; i++)
                         if (i != comm.Rank)
                             comm.Send((input, comm.Rank), i, 0);
+                    */
+                    var body = Encoding.UTF8.GetBytes(input);
+
+                    channel.BasicPublish(exchange: string.Empty,
+                                        routingKey: "msgs",
+                                        basicProperties: null,
+                                        body: body);
                 }
             }
         });
 
         private static Task HandleReceive(Intracommunicator comm) => new Task(() =>
         {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "msgs",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
             while (true)
             {
+                /*
                 var (msg, sender) = comm.Receive<(string, int)>(Communicator.anySource, 0);
                 if (!string.IsNullOrEmpty(msg) && sender != comm.Rank)
                     Console.WriteLine($"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} - Process {sender}: {msg}");
+                */
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($" [x] Received {message}");
+                };
+                channel.BasicConsume(queue: "msgs",
+                    autoAck: true,
+                    consumer: consumer);
             }
         });
 
